eaacd912847a1315966f5f94c21a788c66b7d2f1
Kenji Saito <ken-yo@mbr.nifty.com>
2024-10-23 00:15:30 +0900

    Bumps node modules

--- frontend/src/component/GitHubSignInButton.tsx
+++ frontend/src/component/GitHubSignInButton.tsx
@@ -1,35 +1,35 @@
-"use client";
-import * as crypto from "node:crypto";
-import { Inter } from "next/font/google";
-import { v4 as uuidv4 } from "uuid";
+'use client';
+import * as crypto from 'node:crypto';
+import { Inter } from 'next/font/google';
+import { v4 as uuidv4 } from 'uuid';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 const NEXT_PUBLIC_GITHUB_CLIENT_ID = process.env
-	.NEXT_PUBLIC_GITHUB_CLIENT_ID as string;
+  .NEXT_PUBLIC_GITHUB_CLIENT_ID as string;
 
 function signIn() {
-	const uuid = uuidv4();
-	const state = crypto.createHash("sha512").update(uuid).digest("hex");
+  const uuid = uuidv4();
+  const state = crypto.createHash('sha512').update(uuid).digest('hex');
 
-	const params = {
-		client_id: NEXT_PUBLIC_GITHUB_CLIENT_ID,
-		redirect_uri: window.location.href,
-		state,
-		allow_signup: false,
-	};
-	const queryString = Object.entries(params)
-		.map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-		.join("&");
-	window.location.href = `https://github.com/login/oauth/authorize?${queryString}`;
+  const params = {
+    client_id: NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    redirect_uri: window.location.href,
+    state,
+    allow_signup: false,
+  };
+  const queryString = Object.entries(params)
+    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+    .join('&');
+  window.location.href = `https://github.com/login/oauth/authorize?${queryString}`;
 }
 
 export const GitHubSignInButton = () => {
-	return (
-		<button type="button" onClick={signIn} className={inter.className}>
-			Sign in by GitHub
-		</button>
-	);
+  return (
+    <button type="button" onClick={signIn} className={inter.className}>
+      Sign in by GitHub
+    </button>
+  );
 };
 
 export default GitHubSignInButton;

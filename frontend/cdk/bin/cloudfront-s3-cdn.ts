73d964c306fe844ec4b8bdd900234b41a84051cd
Kenji Saito <ken-yo@mbr.nifty.com>
2025-04-07 19:58:44 +0900

    Bumps node modules

--- frontend/cdk/bin/cloudfront-s3-cdn.ts
+++ frontend/cdk/bin/cloudfront-s3-cdn.ts
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import "source-map-support/register";
-import { NextCloudFrontTemplateStack } from "../lib/cloudfront-s3-cdn-stack";
-import { nextJsExport } from "../lib/process/setup";
+import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
+import { NextCloudFrontTemplateStack } from '../lib/cloudfront-s3-cdn-stack';
+import { nextJsExport } from '../lib/process/setup';
 
 const app = new cdk.App();
 
-const env = app.node.tryGetContext("env");
+const env = app.node.tryGetContext('env');
 const appName =
-	app.node.tryGetContext("appkName") ||
-	`${env ? `${env}-` : ""}next-cloudfront-template`;
-const clientId = app.node.tryGetContext("clientId");
-const apiUrl = app.node.tryGetContext("apiUrl");
+	app.node.tryGetContext('appkName') ||
+	`${env ? `${env}-` : ''}next-cloudfront-template`;
+const clientId = app.node.tryGetContext('clientId');
+const apiUrl = app.node.tryGetContext('apiUrl');
 
 nextJsExport({
-	clientId,
-	apiUrl,
+  clientId,
+  apiUrl,
 });
 
 new NextCloudFrontTemplateStack(app, `${appName}-stack`, {
-	appName,
+  appName,
 });
